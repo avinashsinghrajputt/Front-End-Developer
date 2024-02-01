e04f835be2a8a183eaefc66884f6b9f626bf7625
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2024-02-01 09:55:56 +0530

    first commit

---
+++ js/ifelse.js
@@ -0,0 +1,46 @@
+// let value = 10;
+
+// if (value > 10) {
+//   console.log("Value is greater than 10");
+// } else if (value < 10) {
+//   console.log("Value is less than 10");
+// } else {
+//   console.log("Value is equal to 10");
+// }
+
+
+
+
+// function checkNumber(number) {
+//     if (number > 0) {
+//         console.log("The number is positive.");
+//     } else if (number < 0) {
+//         console.log("The number is negative.");
+//     } else {
+//         console.log("The number is zero.");
+//     }
+// }
+// checkNumber(5);     
+// checkNumber(-3);   
+// checkNumber(0);    
+
+
+//control statement
+// switch statement
+
+let dayOfWeek = "Monday";
+switch (dayOfWeek) {
+ case "Sunday":
+   console.log("Today is Sunday!");
+   break;
+ case "Monday":
+   console.log("Today is Monday!");
+   break;
+ case "Thursday":
+   console.log("It's Thursday!");
+   break;
+ default:
+    console.log("I don't know what day it is...");
+}
+
+
