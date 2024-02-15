723e6612a3bcbdc1a32308cf020be488cdaf7528
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2024-02-15 12:42:47 +0530

    up

--- js/ifelse.js
+++ js/ifelse.js
@@ -28,19 +28,38 @@
 //control statement
 // switch statement
 
-let dayOfWeek = "Monday";
-switch (dayOfWeek) {
- case "Sunday":
-   console.log("Today is Sunday!");
-   break;
- case "Monday":
-   console.log("Today is Monday!");
-   break;
- case "Thursday":
-   console.log("It's Thursday!");
-   break;
- default:
-    console.log("I don't know what day it is...");
-}
+// let dayOfWeek = "Monday";
+// switch (dayOfWeek) {
+//  case "Sunday":
+//    console.log("Today is Sunday!");
+//    break;
+//  case "Monday":
+//    console.log("Today is Monday!");
+//    break;
+//  case "Thursday":
+//    console.log("It's Thursday!");
+//    break;
+//  default:
+//     console.log("I don't know what day it is...");
+// }
 
 
+// sum of 1 to 10 natural number using for loop
+// let num = 10;
+// let sum=0;
+// for (let i=1;i<=num;i++) {
+//   sum +=i;
+// }
+// console.log("sum of natural number",sum);
+
+
+
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   sum += i;
+// }
+// console.log("Sum of the first 10 natural numbers:", sum);
+
+// var a=Number(prompt("Enter the value of a: "));
+// var b=Number(prompt("Enter the value of b: "));
+// alert(a+b);
